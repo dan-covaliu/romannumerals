8713b3e69caac98e4560c38b2d915bd865733ed1
Dan Covaliu <dan.covaliu@bdc.ca>
2019-12-03 22:48:52 -0500

    initial commit

---
+++ RomanNumerals/RomanNumeralsConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace RomanNumerals
+{
+    public class RomanNumeralsConverter
+    {
+        internal string[,] romanNumearals =
+        {
+            {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"},
+            {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"},
+            {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"},
+            {"", "M", "MM", "MMM", "", "", "", "", "", ""}
+        };
+
+        public string Convert(int decimalNumber)
+        {
+            if (decimalNumber >= 4000)
+            {
+                throw new ArgumentException("The decimal number must be a positive integer up to 3999!");
+            }
+            int wholeThousands = decimalNumber / 1000;
+            int hundreds = decimalNumber - wholeThousands * 1000;
+            int wholeHundreds = hundreds / 100;
+            int tens = decimalNumber - wholeThousands *1000 - wholeHundreds * 100;
+            int wholeTens = tens / 10;
+            int units = decimalNumber - wholeThousands * 1000 - wholeHundreds * 100 - wholeTens * 10;
+
+            return romanNumearals[3, wholeThousands] + romanNumearals[2, wholeHundreds] + romanNumearals[1, wholeTens] + romanNumearals[0, units];
+        }
+    }
+}
---
+++ RomanNumeralsTest/RomanNumeralsConverterTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+using RomanNumerals;
+
+namespace RomanNumeralsTest
+{
+    [TestFixture]
+    public class RomanNumeralsConverterTests
+    {
+        [TestCase("I",  1)]
+        [TestCase("II", 2)]
+        [TestCase("V", 5)]
+        [TestCase("L", 50)]
+        [TestCase("LXVII", 67)]
+        [TestCase("XCIX", 99)]
+        [TestCase("CCCLX", 360)]
+        [TestCase("MDCCCXC", 1890)]
+        [TestCase("MMMCMXCIX", 3999)]
+        [TestCase("MMMDLXXXVIII", 3588)]
+        [TestCase("MMM", 3000)]
+        [TestCase("MMCMXCIX", 2999)]
+        public void TestOne(string expected, int actual)
+        {
+            RomanNumeralsConverter romanNumeralsConverter = new RomanNumeralsConverter();
+            string actualString = romanNumeralsConverter.Convert(actual);
+            Assert.AreEqual(expected, actualString);
+        }
+    }
+}
